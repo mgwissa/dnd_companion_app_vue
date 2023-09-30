f7a2cc31399c4dede5816b510f9a8a37cd80ec4b
Mike Wissa <wissam@detroittrading.com>
2023-09-30 10:10:21 -0400

    feat: fix header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -4,26 +4,90 @@ import { RouterLink } from 'vue-router'
 import { useUserStore } from '../stores/user'
 
 const userStore = useUserStore()
-const { isLoggedIn, logIn } = storeToRefs(userStore)
+const { isLoggedIn } = storeToRefs(userStore)
 </script>
 
 <template>
   <header>
     <nav>
-      <div>{{ isLoggedIn }}</div>
       <RouterLink to="/" class="logo">
-        <img src="/DnD-Symbol.png" alt="D&D" width="100" height="100" />
+        <img src="/DnD-Symbol.png" alt="D&D" width="100" />
       </RouterLink>
-      <template v-if="isLoggedIn">
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/contact">Character Sheet</RouterLink>
-        <RouterLink to="/contact">Class</RouterLink>
-        <RouterLink to="/">Backstory</RouterLink>
-      </template>
+      <div class="nav-links" v-if="isLoggedIn">
+        <div>
+          <RouterLink class="nav-link" to="/">Home</RouterLink>
+        </div>
+        <div>
+          <RouterLink class="nav-link" to="/notes">Notes</RouterLink>
+        </div>
+        <div>
+          <a class="nav-link" href="/backstory_sandstone.pdf">Backstory</a>
+        </div>
+        <div>
+          <a class="nav-link" href="https://www.dndbeyond.com" target="_blank">Character Sheet</a>
+        </div>
+        <div>
+          <a class="nav-link" href="https://www.dndbeyond.com" target="_blank">Class</a>
+        </div>
+        <a class="nav-link" @click="userStore.logOut">Logout</a>
+      </div>
       <template v-else>
-        <RouterLink to="/notes">Notes</RouterLink>
-        <button @click="logIn">Login</button>
+        <a class="nav-link" @click="userStore.logIn">Login</a>
       </template>
     </nav>
   </header>
 </template>
+
+<style scoped>
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
+  background-color: #333;
+  color: #fff;
+
+  .nav-link {
+    color: #fff;
+    cursor: pointer;
+    padding: 0 1rem;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    &.active {
+      text-decoration: underline;
+    }
+
+    &:visited {
+      color: #fff;
+    }
+  }
+
+  a {
+    color: #fff;
+    padding: 0 1rem;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    &.active {
+      text-decoration: underline;
+    }
+
+    &:visited {
+      color: #fff;
+    }
+  }
+}
+
+.nav-links {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
--- src/views/NotesView.vue
+++ src/views/NotesView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="about">
     <h1>This is the Notes page</h1>
+    <textarea rows="10" cols="30"></textarea>
+    <div>
+      <h2>Search:</h2>
+      <input type="text" id="search" name="search" placeholder="Search.." />
+    </div>
   </div>
 </template>
