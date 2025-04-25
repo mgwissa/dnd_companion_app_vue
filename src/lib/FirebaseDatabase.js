1b380bebcb7b8302275a622b7e0b770ab51d9d63
Mike Wissa <wissam@detroittrading.com>
2025-04-25 10:26:21 -0400

    remove current project

--- src/lib/FirebaseDatabase.js
+++
@@ -1,9 +0,0 @@
-// import firebase from 'firebase/app'
-import { Firestore } from 'firebase/firestore'
-
-const db = Firestore()
-const notesCollection = db.collection('notes')
-
-export const addNoteToDatabase = (note) => {
-  notesCollection.add(note)
-}
